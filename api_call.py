3e0c991dd576a81ea95d50b031780738339dc949
jose <joseantonio.torre@MAC-Jose-Antonio.local>
2022-07-14 19:30:00 +0200

    new

--- api_call.py
+++ api_call.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-data = {'f1': 3, 'f2': 5, 'f3': 1 , 'f4':0}
+data = {'f1': 30, 'f2': 5, 'f3': 1 , 'f4':0}
 URL = 'http://127.0.0.1:5000/predict'
 
 result = requests.post(URL, json.dumps(data))
